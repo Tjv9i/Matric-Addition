5c3ea97bed33358f8b30cd7dbc3b9b79e054cdb1
Tjv9i <69515737+Tjv9i@users.noreply.github.com>
2020-08-29 19:25:19 +0530

    Create Matrixadd.java

---
+++ Matrixadd.java
@@ -0,0 +1,46 @@
+
+package matricsadd;
+
+
+public class MatricsAdd 
+{
+
+   
+    public static void main(String[] args) 
+    {
+        int a[][]={{1,3,4},{2,4,3},{3,4,5}};
+        int b[][]={{1,3,4},{2,4,3},{1,2,4}};
+        int c[][]=new int[3][3];
+        System.out.println("Entered matrices are:");
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            { 
+               System.out.print(a[i][j]);
+            }
+        System.out.println();
+        }
+        System.out.println();
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+               System.out.print(b[i][j]);
+            }
+        System.out.println();
+        }
+        
+        System.out.println("The result matrix is:");
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                c[i][j]=a[i][j]+b[i][j];
+                System.out.print(c[i][j]);
+            }
+        System.out.println();
+        }
+    }
+    
+    
+}
